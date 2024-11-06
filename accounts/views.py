d03e8c1491db2890df2065839d103e61700a22dd
TechBuilderTeam <saashid18@gmail.com>
2024-11-06 23:24:03 +0600

    added apps(products, category and accounts

--- accounts/views.py
+++ accounts/views.py
@@ -115,8 +115,8 @@ class UserRegistration(APIView):
 
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f'http://127.0.0.1:8000/accounts/activate/{uid}/{token}/'
-            # confirm_link = f'http://{current_site.domain}/accounts/activate/{uid}/{token}/'
+            # confirm_link = f'http://127.0.0.1:8000/accounts/activate/{uid}/{token}/'
+            confirm_link = f'http://{current_site.domain}/accounts/activate/{uid}/{token}/'
 
             email_subject = 'Confirm your email'
             email_body = render_to_string(
